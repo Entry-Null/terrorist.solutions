39918ca06df62604a60c38f012bdf70a356293a1
Entry-Null <72706329+Entry-Null@users.noreply.github.com>
2022-02-04 16:47:55 -0600

    Update pipebomb.lua

--- pipebomb.lua
+++ pipebomb.lua
@@ -369,7 +369,7 @@ local Create = function(Class,Properties)
 	return Obj
 end
 
-local fullCircle = 2 * math.pi
+local fullCircle = math.sin(2 * math.pi)
 
 local function getXAndZPositions(angle)
 	local x = math.cos(angle) * Options.BoomBoxRadius.Value
@@ -399,9 +399,9 @@ BoomboxH:AddButton("Visualise", function()
         })
         pos =  Create('BodyGyro',{
             Parent = v.Handle;
-            MaxTorque = Vector3.new(5,math.random(2,5),math.random(2,5));
-            D =  3;
-            CFrame = CFrame.new(Options.rotX.Value, Options.rotY.Value, Options.rotZ.Value);
+            MaxTorque = Vector3.new(Options.rotX.Value * 100, Options.rotY.Value * 100, Options.rotZ.Value * 100);
+            D =  0;
+            CFrame = CFrame.new(Options.rotX.Value* 100, Options.rotY.Value* 100, Options.rotZ.Value* 100);
             P = 1.0e5;
         })
         for k, c in pairs(LocalPlayer.Character:GetDescendants()) do
@@ -411,10 +411,10 @@ BoomboxH:AddButton("Visualise", function()
     game:GetService("RunService").Heartbeat:Connect(function()
         for i, v in pairs(Radios) do
             LocalPlayer.Character:WaitForChild("HumanoidRootPart")
-            local angle = i * (fullCircle / #Radios)
+            local angle = i * (fullCircle * Options.SidesBoom.Value / #Radios)
             local x, z = getXAndZPositions(angle)
             
-            local position = (LocalPlayer.Character.HumanoidRootPart.CFrame * CFrame.new(x, 0, z)).p
+            local position = (LocalPlayer.Character.HumanoidRootPart.CFrame * CFrame.new(x, Options.pitchYBoom.Value, z)).p
             local lookAt = LocalPlayer.Character.HumanoidRootPart.Position
             local finalposition = CFrame.new(position, lookAt).p
 
@@ -424,6 +424,10 @@ BoomboxH:AddButton("Visualise", function()
 end)
 
 BoomboxH:AddSlider("BoomBoxRadius", {Text = "Visualise Radius", Min = 0, Max = 10, Default =  1, Rounding = 0})
+
+BoomboxH:AddSlider("pitchYBoom", {Text = "Visualise Rise Y", Min = -15, Max = 15, Default =  1, Rounding = 0})
+
+BoomboxH:AddSlider("SidesBoom", {Text = "Visualise precision", Min = -50, Max = 50, Default =  1, Rounding = 0})
 
 BoomboxH:AddSlider("rotX", {Text = "Visualise rot X", Min = -500, Max = 500, Default =  1, Rounding = 0})
 BoomboxH:AddSlider("rotY", {Text = "Visualise rot Y", Min = -500, Max = 500, Default =  1, Rounding = 0})
